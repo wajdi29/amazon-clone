333fa483bc7489098afdaf92c7d46120736ff8dc
wajdi29 <wajdidouiri26@gmail.com>
2021-01-27 12:59:19 +0100

    home and header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Header from './Header'
+
+import Header from './Header';
+
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
 
-      { /* Home*/}
     </div>
   );
 }
