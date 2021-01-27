3a13dcc6c64e833334fe9084235ddcb23db28923
wajdi29 <wajdidouiri26@gmail.com>
2021-01-27 09:46:43 +0100

    init commit

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='' />
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                {/*logo*/}
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineOne">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineOne">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineOne">Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2GEPgDVp4-oqFUn79F8x4d_R0WBumeg4",
+    authDomain: "clone-fe780.firebaseapp.com",
+    projectId: "clone-fe780",
+    storageBucket: "clone-fe780.appspot.com",
+    messagingSenderId: "78764622738",
+    appId: "1:78764622738:web:d1561170ebd0be6c18374b",
+    measurementId: "G-6SS251S1L7"
+};
