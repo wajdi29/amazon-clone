c7a166d40c102bf28c5d0292e64791afd4702d18
wajdi29 <wajdidouiri26@gmail.com>
2021-01-28 12:32:02 +0100

    React  router checkout page , subtotal component and styles

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Header from './Header';
 import Home from './Home';
 
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Checkout from './Checkout';
+
+
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path='/checkout'>
 
-    </div>
+            <Checkout />
+          </Route>
+          <Route path='/'>
+
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import "./Product.css"
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product__info">
-            <p>The lean startup</p>
-            <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-            </p>
-            <div className="product__rating">
-                <p>⭐</p>
-               
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>🌟</p>
+                        ))}
+
+
+                </div>
             </div>
-            </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="" />
+            <img src={image} alt="" />
             <button>Add to basket</button>
         </div>
     )
